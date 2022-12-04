bcf011c5f7e045e4fe0c6bf4e61b7d51c18ea2c5
Sylvère Grégeois <sylvere.gregeois@outlook.com>
2022-12-04 20:05:48 +0100

    tray icon

--- src/fps_counter.py
+++ src/fps_counter.py
@@ -1,12 +1,30 @@
 import time
 import gamesense
-
 from fps_inspector_sdk import fps_inspector
 from win32gui import GetForegroundWindow
-
 from win32process import GetWindowThreadProcessId
+from PIL import Image, ImageDraw
+from pystray import Icon, Menu, MenuItem
+import threading
 
-def run():
+def on_quit_callback(systray):
+    exit()
+
+def create_image(width, height, color1, color2):
+    # Generate an image and draw a pattern
+    image = Image.new('RGB', (width, height), color1)
+    dc = ImageDraw.Draw(image)
+    dc.rectangle(
+        (width // 2, 0, width, height // 2),
+        fill=color2)
+    dc.rectangle(
+        (0, height // 2, width // 2, height),
+        fill=color2)
+
+    return image
+
+
+def start_counter():
     gs = gamesense.GameSense("FPS_COUNTER", "FPS Counter")
 
     register_resp = gs.register_game(icon_color_id=gamesense.GS_ICON_GOLD)
@@ -43,10 +61,7 @@ def run():
     if not event_resp.success:
         print(event_resp.data)
 
-
-    float_formatter = lambda x: "%.5f" % x
-
-    while True:
+    while not exit_event.is_set():
         pid = int (GetWindowThreadProcessId(GetForegroundWindow())[1])
         fps_inspector.start_fliprate_recording (pid)
         time.sleep (0.5)
@@ -72,9 +87,35 @@ def run():
             if not send_hb_resp.success:
                 print(send_hb_resp.data)
 
+def exit(icon: Icon) -> None:
+    icon.visible = False
+    exit_event.set()
+    icon.stop()
+
+def setup(icon: Icon) -> None:
+    icon.visible = True  # Required to make the systray icon show up
+
+    while not exit_event.is_set():  # This exits the loop if exit is ever set -> program was quit
+         start_counter()
+         exit_event.wait(5)  # allows exiting while waiting. time.sleep would block
 
 def main ():
-    run()
+    # This event is used to stop the loop.
+    global exit_event
+    exit_event = threading.Event()
+
+    # Create and run the icon
+    icon = Icon('Amazing Systray')
+    icon.menu = Menu(
+        MenuItem('Quit', exit)
+    )
+    icon.icon = create_image(64, 64, 'black', 'white')
+
+    # with Image.open('assets\\image\\meter.ico') as im:
+    #     im.load()
+    #     icon.icon = im
+
+    icon.run(setup=setup)
 
 if __name__ == "__main__":
     main ()
