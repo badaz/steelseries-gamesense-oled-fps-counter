54d6d6becb583ed68ec4ba97b1d3a1e23f0d136a
Sylvère Grégeois <sylvere.gregeois@outlook.com>
2022-12-09 12:46:51 +0100

    Bump version: 0.1.5 → 0.1.6

--- release.bat
+++ release.bat
@@ -1,1 +1,1 @@
-gh release create v0.1.5 ".\dist\Steelseries Gamesense OLED FPS Counter.exe" -F .\CHANGELOG.md
+gh release create v0.1.6 ".\dist\Steelseries Gamesense OLED FPS Counter.exe" -F .\CHANGELOG.md
